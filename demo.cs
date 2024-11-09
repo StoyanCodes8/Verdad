bbd0d7c6b56dd64af3ce233b047365fdf9d68411
StoyanCodes8 <stoyanstefanov26@gmail.com>
2024-11-09 20:45:20 +0200

    Initial project setup for PoliticoMind

---
+++ demo.cs
@@ -0,0 +1,13 @@
+using System.Globalization;
+
+namespace Demonstration
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Beggining")
+        }
+    }
+}
